7076a24fc2612da2461a3989d0101831ba106858
DevriesL <therkduan@gmail.com>
2018-12-31 21:42:21 +0800

    disp: msm: dsi: add get_main_display
    Change-Id: Id670875adc18659521c424a716cf0ef194f9627f
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_display.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_display.c
@@ -61,11 +61,6 @@ static const struct of_device_id dsi_display_dt_match[] = {
 };
 
 struct dsi_display *primary_display;
-struct dsi_display *get_primary_display(void)
-{
-	return primary_display;
-}
-EXPORT_SYMBOL(get_primary_display);
 
 static void dsi_display_mask_ctrl_error_interrupts(struct dsi_display *display,
 			u32 mask, bool enable)
@@ -7935,6 +7930,10 @@ int dsi_display_unprepare(struct dsi_display *display)
 
 	SDE_EVT32(SDE_EVTLOG_FUNC_EXIT);
 	return rc;
+}
+
+struct dsi_display *get_main_display(void) {
+	return primary_display;
 }
 
 static int __init dsi_display_register(void)
